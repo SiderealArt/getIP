fda1d1193a28de76bd7c776dc6180fe363cd9500
SiderealArt <nelson22768384@gmail.com>
2021-08-17 21:56:19 +0800

    Update Home.vue

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -18,6 +18,7 @@
         </h1>
         <div class="flex">
           <input
+          minlength="7" maxlength="15" size="15" pattern="^((\d{1,2}|1\d\d|2[0-4]\d|25[0-5])\.){3}(\d{1,2}|1\d\d|2[0-4]\d|25[0-5])$"
             v-model="query"
             type="text"
             class="
